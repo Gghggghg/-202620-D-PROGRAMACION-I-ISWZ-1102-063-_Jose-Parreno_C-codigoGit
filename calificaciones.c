71584c926edca78132086666464aa5e4dd1f9e3e
Igor <josedupagghg@gmail.com>
2026-04-12 17:48:50 -0500

    calificaciones.c

---
+++ calificaciones.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define ESTUDIANTES 2
+#define ASIGNATURAS 2
+
+int main() {
+    float calificaciones[ESTUDIANTES][ASIGNATURAS];
+    float promedioEstudiante[ESTUDIANTES];
+    float promedioAsignatura[ASIGNATURAS];
+    float maxEstudiante[ESTUDIANTES], minEstudiante[ESTUDIANTES];
+    float maxAsignatura[ASIGNATURAS], minAsignatura[ASIGNATURAS];
+    int aprobados[ASIGNATURAS], reprobados[ASIGNATURAS];
+    int estudianteMaxAsignatura[ASIGNATURAS], estudianteMinAsignatura[ASIGNATURAS];
+
+
+    return 0;
+}
+
